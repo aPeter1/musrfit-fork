6fe8f1c8cfa35962d8e6d5b04bc109318098fe0d
nemu <nemu>
2009-06-16 06:29:33 +0000

    some more adaptation to the new Makefiles

--- src/classes/PStartupHandler.cpp
+++ src/classes/PStartupHandler.cpp
@@ -67,8 +67,8 @@ PStartupHandler::PStartupHandler()
     // check if the MUSRFITPATH system variable is set
     pmusrpath = getenv("MUSRFITPATH");
     if (pmusrpath == 0) { // not set, will try default one
-      home = getenv("HOME");
-      sprintf(musrpath, "%s/analysis/bin", home);
+      home = getenv("ROOTSYS");
+      sprintf(musrpath, "%s/bin", home);
       cout << endl << "**WARNING** MUSRFITPATH environment variable not set will try " << musrpath << endl;
     } else {
       strncpy(musrpath, pmusrpath, sizeof(musrpath));
