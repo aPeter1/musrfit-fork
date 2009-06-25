215a9afa043cbf7af1355ca90e6f0dc2475f0e6b
Bastian M. Wojek <basti@kth.se>
2009-06-25 18:34:08 +0000

    Added the musrfit --title-from-data-file option to msr2data (-t) and made TFitPofB compilable with g++-4.4

---
+++ src/external/TFitPofB-lib/include/TSkewedGssLinkDef.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+
+  TSkewedGssLinkDef.h
+
+  Author: Bastian M. Wojek
+  e-mail: bastian.wojek@psi.ch
+
+  2009/01/24
+
+***************************************************************************/
+
+// root dictionary stuff --------------------------------------------------
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TSkewedGss+;
+
+#endif //__CINT__
+// root dictionary stuff --------------------------------------------------
+
