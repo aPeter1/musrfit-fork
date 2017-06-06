c6cee9f6d80a16cbcbb1a5570306dcb7c6bb8851
Andreas Suter <andreas.suter@psi.ch>
2017-06-06 10:49:43 +0200

    fixed wrong month in the data conversion.

--- src/classes/PRunDataHandler.cpp
+++ src/classes/PRunDataHandler.cpp
@@ -6154,7 +6154,7 @@ bool PRunDataHandler::DateToISO8601(string inDate, string &iso8601Date)
     return false;
 
   TString str("");
-  str.Form("%04d-%02d-%02d", 1900+tm.tm_year, tm.tm_mon, tm.tm_mday);
+  str.Form("%04d-%02d-%02d", 1900+tm.tm_year, tm.tm_mon+1, tm.tm_mday);
 
   iso8601Date = str.Data();
 
