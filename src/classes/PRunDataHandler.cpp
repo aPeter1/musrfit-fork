80a73983509d5829128bf1363abcaae3f99103be
Andreas Suter <andreas.suter@psi.ch>
2017-06-06 12:15:17 +0200

    found another date issue which is fixed now.

--- src/classes/PRunDataHandler.cpp
+++ src/classes/PRunDataHandler.cpp
@@ -6187,7 +6187,7 @@ void PRunDataHandler::SplitTimeDate(TString timeData, TString &time, TString &da
   }
 
   time = TString::Format("%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
-  date = TString::Format("%04d-%02d-%02d", tm.tm_year+1900, tm.tm_mon, tm.tm_mday);
+  date = TString::Format("%04d-%02d-%02d", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday);
 }
 
 //--------------------------------------------------------------------------
