cd82c613aab28ba9198470ee3f322421c5b3172b
Andreas Suter <andreas.suter@psi.ch>
2018-06-04 09:29:16 +0200

    added missing header files.

--- src/classes/PRunMuMinus.cpp
+++ src/classes/PRunMuMinus.cpp
@@ -27,6 +27,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #ifdef HAVE_GOMP
 #include <omp.h>
 #endif
