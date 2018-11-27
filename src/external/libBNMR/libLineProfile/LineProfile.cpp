c5c5b16c462b1ecfe7206ddc8c6c2e9ba390cc67
Andreas Suter <andreas.suter@psi.ch>
2018-11-27 12:25:18 +0100

    BNMR lib installation problems
    Adopted the build system for the BNMR libs such that the generated files are found and can be installed. The BNMR lib mantainers (ZS, JK) are responsible for the functionality though.

--- src/external/libBNMR/libLineProfile/LineProfile.cpp
+++ src/external/libBNMR/libLineProfile/LineProfile.cpp
@@ -20,7 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
 
-#include "libLineProfile.h"
+#include "LineProfile.h"
 #include <iostream> //for testing purposes
 
 //Implement helperfunctions
