c6766864d90ea4154d5d4e138910654ce56c7166
Andreas Suter <andreas.suter@psi.ch>
2018-12-11 13:59:58 +0100

    AddT0 offset bug
    Up to now there has been an addT0 offset bug which resulted to the following situation.
    Since the offset was wrong by '1', for the first addt0 the t0 of the file was used
    instead of the one given in the msr-file. If these t0's are substantially different, the
    adding of the runs was quite off, resulting in strong distortions at very early times.
    Pant Amba Datt pointed towards this bug which is fixed now.

--- src/classes/PRunAsymmetryRRF.cpp
+++ src/classes/PRunAsymmetryRRF.cpp
@@ -1278,8 +1278,8 @@ Bool_t PRunAsymmetryRRF::GetProperT0(PRawRunData* runData, PMsrGlobalBlock *glob
       }
 
       // fill in the T0's from the msr-file (if present)
-      for (Int_t j=0; j<fRunInfo->GetAddT0BinSize(i); j++) {
-        fAddT0s[i-1][j] = fRunInfo->GetAddT0Bin(i, j);
+      for (Int_t j=0; j<fRunInfo->GetAddT0BinSize(i-1); j++) {
+        fAddT0s[i-1][j] = fRunInfo->GetAddT0Bin(i-1, j);
       }
 
       // fill in the T0's from the data file, if not already present in the msr-file
