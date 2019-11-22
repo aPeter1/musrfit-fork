55b681acc18ce3a2aa41184b44177907d4f1b05f
Andreas Suter <andreas.suter@psi.ch>
2019-11-22 18:15:09 +0100

    replaced decrepitated OS system check under Qt. The new version works for Qt >= 5.4 which should cover everything still alive.

--- src/musredit_qt5/musredit/PAdmin.cpp
+++ src/musredit_qt5/musredit/PAdmin.cpp
@@ -1062,12 +1062,12 @@ void PAdmin::createMusreditStartupFile()
     line.replace("@prefix@", MUSRFIT_PREFIX);
     line.replace("@DOCDIR@", MUSRFIT_DOC_DIR);
     if (line.contains("<font_name>")) { // defaults: linux: Monospace, macOS: Courier New
-      if (QSysInfo::macVersion() != QSysInfo::MV_None) {
+      if (QSysInfo::productType().contains("osx")) {
         line.replace("Monospace", "Courier New");
       }
     }
     if (line.contains("<font_size>")) { // defaults: linux: 12, macOS: 16
-      if (QSysInfo::macVersion() != QSysInfo::MV_None) {
+      if (QSysInfo::productType().contains("osx")) {
         line.replace("12", "16");
       }
     }
